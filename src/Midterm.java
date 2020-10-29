76f893beaed3c264ab4d2d6346df0c973466e0cb
Michael Medling <mikeymedling31@gmail.com>
2020-10-29 10:20:16 -0400

    final sentence

--- src/Midterm.java
+++ src/Midterm.java
@@ -3,7 +3,10 @@ public class Midterm {
 
 	public static void main(String[] args) {
 		System.out.println("Once upon a time in the mystical land of Shangrila there dwelt an immortal named Kai-Yun and his trusty band of ninja monkeys.\n");
-		System.out.println("They were very clever monkeys indeed, known for their ingenious teamwork.");
+		System.out.println("They were very clever monkeys indeed, known for their ingenious teamwork ");
+		System.out.println("and their natural magical prowess. Once they were challenged by a swarm of seemingly unstoppable Shawlin battle bugs, but with their "
+				+ "magical powers they paired up and mowed down their foes. One-by-one every last pesky bug fell at the quick hands of the monkeys.\r\n"
+				+ "");
 	}
 
 }
