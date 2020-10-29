8021e996c172c4b6081533d8f516d3d05cdb0079
jessperschke <anonymous171@posteo.de>
2020-10-29 11:27:35 -0400

    fixed stash,monkeys to students

--- src/Midterm.java
+++ src/Midterm.java
@@ -2,14 +2,14 @@
 public class Midterm {
 
 	public static void main(String[] args) {
-		System.out.println("Once upon a time in the mystical land of Detroit there dwelt an immortal named David and his trusty band of ninja monkeys.\n");
+		System.out.println("Once upon a time in the mystical land of Detroit there dwelt an immortal named David and his trusty band of ninja students.\n");
 		System.out.println("They were very clever monkeys indeed, known for their ingenious teamwork ");
 		System.out.println("and their natural magical prowess. Once they were challenged by a swarm of seemingly unstoppable Shawlin battle bugs, but with their "
-				+ "magical powers they paired up and mowed down their foes. One-by-one every last pesky bug fell at the quick hands of the monkeys.\r\n"
+				+ "magical powers they paired up and mowed down their foes. One-by-one every last pesky bug fell at the quick hands of the students.\r\n"
 				+ "");
-		System.out.println("After this fight, the monkeys chose peace and freedom.");
-		System.out.println("And the monkeys retired, and decided to form a band.");
+		System.out.println("And the students retired, and decided to form a band.");
 		System.out.println("They called themselves The Magical Monks!!");
+		System.out.println("After this fight, the students chose peace and freedom.");
 	}
 
 }
