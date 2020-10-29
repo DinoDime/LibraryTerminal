357c1102f339e8c283a06a7e05169fba7e966b89
Michael Medling <mikeymedling31@gmail.com>
2020-10-29 14:12:07 -0400

    Movies class

--- src/Midterm.java
+++ src/Midterm.java
@@ -4,7 +4,7 @@ public class Midterm {
 	public static void main(String[] args) {
 		System.out.println(
 				"Once upon a time in the mystical land of Detroit there dwelt an immortal named David and his trusty band of ninja students.\n");
-		System.out.println("They were very clever monkeys indeed, known for their ingenious teamwork ");
+		System.out.println("They were very clever students indeed, known for their ingenious teamwork ");
 		System.out.println(
 				"and their natural magical prowess. Once they were challenged by a swarm of seemingly unstoppable Shawlin battle bugs, but with their "
 						+ "magical powers they paired up and mowed down their foes. One-by-one every last pesky bug fell at the quick hands of the students.\r\n"
---
+++ src/Movies.java
@@ -0,0 +1,42 @@
+
+public class Movies extends Media{
+	private String director;
+	
+	
+	
+	public Movies() {
+		
+	}
+	
+	public Movies(String director) {
+		super();
+		this.director = director;
+	}
+
+
+	public String getDirector() {
+		return director;
+	}
+
+
+	public void setDirector(String director) {
+		this.director = director;
+	}
+
+
+	@Override
+	public Status displayStatus() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String toString() {
+		return "Movies [director=" + director + ", displayStatus()="
+				+ displayStatus() + ", getTitle()=" + getTitle() + ", getDueDate()=" + getDueDate() + ", toString()="
+				+ super.toString() + "]";
+	}
+	
+	
+	
+}
