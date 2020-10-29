502527c91661c167ce73d59b0e443c77d2800226
jessperschke <anonymous171@posteo.de>
2020-10-29 10:02:41 -0400

    recommit stuff

---
+++ src/Midterm.java
@@ -0,0 +1,8 @@
+
+public class Midterm {
+
+	public static void main(String[] args) {
+		System.out.println("Once upon a time in the mystical land of Shangrila there dwelt an immortal named Kai-Yun and his trusty band of ninja monkeys.\n");
+	}
+
+}
