5c27384aff54bff32f4a37450f78f8983ed1b1f9
Michael Medling <mikeymedling31@gmail.com>
2020-10-31 12:35:16 -0400

    chekout validate

--- src/Midterm.java
+++ src/Midterm.java
@@ -8,12 +8,12 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Collections;
 import java.util.Date;
+import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 import java.io.*;
 import java.nio.*;
 import java.nio.channels.FileChannel;
-
 
 public class Midterm {
 	static List<Book> things = new ArrayList<>();
@@ -22,14 +22,15 @@ public class Midterm {
 	private static Path filePath = Paths.get("BookList.txt");
 
 	public static void main(String[] args) {
-		while(true) {
-		System.out.println("Welcome to the Library index");
+		while (true) {
+			System.out.println("Welcome to the Library index");
 			System.out.println("\nWhat would you like to do?");
-			System.out.println("1)List all books\n2)Search for a book by Title/Keyword or Author\n3)Return a rental\n4)Quit");
+			System.out.println(
+					"1)List all books\n2)Search for a book by Title/Keyword or Author\n3)Return a rental\n4)Quit");
 			int choice = input.nextInt();
 			if (choice == 1) {
 				readFile();
-				input.nextLine();
+				// input.nextLine();
 				bookCheckOut();
 				break;
 			} else if (choice == 2) {
@@ -41,23 +42,21 @@ public class Midterm {
 				input.nextLine();
 				bookReturn();
 				break;
-				
-				//System.out.println("What are you returning?");
-				//String book = input.next();
-				//boolean status = true;
-				//returnBooks.bookReturn(book, status);
-			}
-			else if (choice == 4) {
+
+				// System.out.println("What are you returning?");
+				// String book = input.next();
+				// boolean status = true;
+				// returnBooks.bookReturn(book, status);
+			} else if (choice == 4) {
 				System.out.println("Goodbye");
 				break;
-			}
-			else {
+			} else {
 				System.out.println("Invalid entry. Please try again.");
 				continue;
 			}
 		}
-}
-		
+	}
+
 //**************List Method*******************************************
 	public static List<Book> readFile() {
 		try {
@@ -70,7 +69,7 @@ public class Midterm {
 				boolean status = Boolean.parseBoolean(parts[1]);
 				String author = parts[2];
 				things.add(new Book(title, status, author));
-				System.out.printf("%-23s",title);
+				System.out.printf("%-23s", title);
 				System.out.println(" by " + author);
 			}
 			System.out.println("========================================");
@@ -100,40 +99,37 @@ public class Midterm {
 
 //***************Checkout Book Method**************************************************	
 	public static void bookCheckOut() {
-		System.out.println("What book would you like to checkout?");
-		String checkoutItem = input.nextLine().toLowerCase();
-
-		for (Book book : things) {
-			if (book.getTitle().toLowerCase().contains(checkoutItem)) {
-				if (book.getStatus() == true) {
-					System.out.println(book.toString() + " is available");
-					System.out.println("You have checked out " + book.toString());
-					book.setStatus(false);
-					dueDate();
-					truncateFile();
-					rewriteFile();
-					//System.out.println(readFile());
-					
+		int a = 0;
+		input.nextLine();
+		do {
+			try {
+				System.out.println("What book would you like to checkout?");
+				String checkoutItem = input.nextLine().toLowerCase();
+				if (checkoutItem.matches("[a-zA-Z]+")) {
+					for (Book book : things) {
+						if (book.getTitle().toLowerCase().contains(checkoutItem)
+								|| book.getAuthor().toLowerCase().contains(checkoutItem)) {
+							if (book.getStatus() == true) {
+								System.out.println(book.toString() + " is available");
+								System.out.println("You have checked out " + book.toString());
+								book.setStatus(false);
+								dueDate();
+								truncateFile();
+								rewriteFile();
+								a = 1;
+							} else {
+								System.out.println(book.toString() + " is checked out and therefore unavailable");
+							}
+						}
+					}
 				} else {
-					System.out.println(book.toString() + " is checked out and therefore unavailable");
-
+					System.out.println("Please try again.");
 				}
-
-			} else if (book.getAuthor().toLowerCase().contains(checkoutItem)) {
-				if (book.getStatus() == true) {
-					System.out.println(book.toString() + " is available");
-					System.out.println("You have checked out " + book.toString());
-					book.setStatus(false);
-					dueDate();
-				} else {
-					System.out.println(book.toString() + " is checked out and therefore unavailable");
-					
-				}
-
+			} catch (InputMismatchException e) {
+				System.out.println("Please try again.");
 			}
-		}
-
-		}
+		} while (a == 0);
+	}
 
 //***************Return Book Method**************************************************	
 	public static void bookReturn() {
@@ -150,14 +146,13 @@ public class Midterm {
 					truncateFile();
 					rewriteFile();
 					System.out.println("Thank you for returning " + book.toString());
-					}
+				}
 			}
 		}
 	}
-					
-					
-					//System.out.println(readFile());
-					
+
+	// System.out.println(readFile());
+
 //				} else {
 //					System.out.println(book.toString() + " is checked out and therefore unavailable");
 //
@@ -179,55 +174,51 @@ public class Midterm {
 //
 //		}
 
-	
-	
-	
 //***************Due Date Method**************************************************	
-		public static void dueDate() {
-			// get a calendar instance, which defaults to "now"
-		    Calendar calendar = Calendar.getInstance();
-		    
-		    // get a date to represent "today"
-		    Date today = calendar.getTime();
-		    System.out.println("Today is " + today + ".");
-		 
-		    // add 14 days to the date/calendar
-		    calendar.add(Calendar.DAY_OF_YEAR, 14);
-		    
-		    // now get "due date"
-		    Date dueDate= calendar.getTime();
+	public static void dueDate() {
+		// get a calendar instance, which defaults to "now"
+		Calendar calendar = Calendar.getInstance();
 
-		    // print out tomorrow's date
-		    System.out.println("Your due date is " + dueDate + ".");
-		}
-		
-			
+		// get a date to represent "today"
+		Date today = calendar.getTime();
+		System.out.println("Today is " + today + ".");
+
+		// add 14 days to the date/calendar
+		calendar.add(Calendar.DAY_OF_YEAR, 14);
+
+		// now get "due date"
+		Date dueDate = calendar.getTime();
+
+		// print out tomorrow's date
+		System.out.println("Your due date is " + dueDate + ".");
+	}
+
 //***************Truncate Method**************************************************	
-		public static void truncateFile() {
-			try {
-				FileChannel.open(Paths.get("BookList.txt"), StandardOpenOption.WRITE).truncate(0).close();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			
+	public static void truncateFile() {
+		try {
+			FileChannel.open(Paths.get("BookList.txt"), StandardOpenOption.WRITE).truncate(0).close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-			
+
+	}
+
 //***************Write Method**************************************************	
-	public static void rewriteFile() {   
-			try {
-		      FileWriter myWriter = new FileWriter("BookList.txt");
-		      for(Book rewriteList: things) {
-		    	  String jkadf = (rewriteList.getTitle() + "~" + rewriteList.getStatus() + "~" + rewriteList.getAuthor() + "\n");
-		      myWriter.write(jkadf);
-		      }
-		      myWriter.close();
-		      System.out.println("Successfully wrote to the file.");
-		    } catch (IOException e) {
-		      System.out.println("An error occurred.");
-		      e.printStackTrace();
-		    }
-	
-		
+	public static void rewriteFile() {
+		try {
+			FileWriter myWriter = new FileWriter("BookList.txt");
+			for (Book rewriteList : things) {
+				String jkadf = (rewriteList.getTitle() + "~" + rewriteList.getStatus() + "~" + rewriteList.getAuthor()
+						+ "\n");
+				myWriter.write(jkadf);
+			}
+			myWriter.close();
+			System.out.println("Successfully wrote to the file.");
+		} catch (IOException e) {
+			System.out.println("An error occurred.");
+			e.printStackTrace();
+		}
+
 	}
 }
